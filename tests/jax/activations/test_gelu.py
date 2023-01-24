8f80d0466aa5620b64e5e2f75ccb6f956a8a9d3a
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-01-24 22:26:13 +0000

    feat: add gelu activation

---
+++ tests/jax/activations/test_gelu.py
@@ -0,0 +1,19 @@
+"""Test GeLU activation function module"""
+
+import numpy as np
+
+from jax import random
+from src.jax.activations.gelu import GeLU
+
+
+def test_gelu() -> None:
+    """Test GeLU activation function"""
+    temp_array = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+
+    gelu = GeLU()
+    variables = gelu.init(random.PRNGKey(0), x=temp_array)
+
+    assert np.allclose(
+        gelu.apply(variables, x=temp_array),
+        np.array([0.0, 0.841192, 1.9545977, 2.9963627, 3.99993, 4.9999995]),
+    )
