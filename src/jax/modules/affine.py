189a0ad36498e323fe0878cae7baf2e326d36316
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-01-24 22:26:48 +0000

    feat: add affine layer

---
+++ src/jax/modules/affine.py
@@ -0,0 +1,29 @@
+"""Affine Transformation Module in Flax"""
+
+from flax import linen as nn
+
+from jax._src.typing import ArrayLike
+
+# local imports
+from src.jax.utils import ones, zeros
+
+__all__ = ["Affine"]
+
+
+class Affine(nn.Module):
+    """
+    A Flax linen Module to perform a Affine Transformation
+
+    Attributes:
+        dim: Needed to generate matrices of the appropriate shape
+    """
+
+    dim: int = 512
+
+    def setup(self) -> None:
+        self.alpha = self.param("alpha", ones, (1, 1, self.dim))
+        self.beta = self.param("beta", zeros, (1, 1, self.dim))
+
+    @nn.compact
+    def __call__(self, x: ArrayLike, *args, **kwargs) -> ArrayLike:
+        return x * self.alpha + self.beta
---
+++ tests/jax/modules/test_affine.py
@@ -0,0 +1,28 @@
+"""Test Affine Flax module"""
+
+import numpy as np
+import pytest
+
+from jax import random
+from src.jax.modules.affine import Affine
+from src.jax.utils import ones
+
+
+@pytest.mark.parametrize(
+    argnames="dim, seed",
+    argvalues=[
+        (512, 0),
+        (1024, 42),
+    ],
+    ids=["dim-512-seed-0", "dim-1024-seed-42"],
+)
+def test_affine(dim: int, seed: int) -> None:
+    """Test Affine module"""
+    temp_array = ones(key=random.PRNGKey(seed), shape=(1, 1, dim))
+    affine = Affine(dim=dim)
+    variables = affine.init(random.PRNGKey(seed), x=temp_array)
+
+    assert np.allclose(
+        affine.apply(variables, x=temp_array),
+        np.ones(shape=(1, 1, dim)),
+    )
