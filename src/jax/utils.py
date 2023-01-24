14da9f38928e661e67acc826249be3459bc2e80f
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-01-24 22:25:42 +0000

    chore: update configs

---
+++ src/jax/utils.py
@@ -0,0 +1,35 @@
+"""JAX utility functions"""
+from typing import Optional, Tuple, Union
+
+import jax.numpy as jnp
+from jax._src.typing import ArrayLike, DTypeLike
+
+__all__ = ["full", "ones", "zeros"]
+
+
+def full(
+    key,  # pylint: disable=unused-argument
+    shape: Union[int, Tuple[int, ...]],
+    fill_value: ArrayLike,
+    dtype: Optional[DTypeLike] = None,
+) -> ArrayLike:
+    """Helper function to create a full array of a given shape and dtype."""
+    return jnp.full(shape, fill_value, dtype)
+
+
+def ones(
+    key,  # pylint: disable=unused-argument
+    shape: Union[int, Tuple[int, ...]],
+    dtype: Optional[DTypeLike] = None,
+) -> ArrayLike:
+    """Helper function to create an array of ones of a given shape and dtype."""
+    return jnp.ones(shape, dtype)
+
+
+def zeros(
+    key,  # pylint: disable=unused-argument
+    shape: Union[int, Tuple[int, ...]],
+    dtype: Optional[DTypeLike] = None,
+) -> ArrayLike:
+    """Helper function to create an array of zeros of a given shape and dtype."""
+    return jnp.zeros(shape, dtype)
---
+++ tests/jax/test_utils.py
@@ -0,0 +1,46 @@
+"""Test JAX utility functions"""
+
+import numpy as np
+import pytest
+
+from jax import random
+from src.jax.utils import full, ones, zeros
+
+
+@pytest.mark.parametrize(
+    argnames="seed",
+    argvalues=[0, 42],
+    ids=["seed-0", "seed-42"],
+)
+def test_full(seed: int) -> None:
+    """Test full function"""
+    assert np.allclose(
+        full(key=random.PRNGKey(seed), shape=(1, 1, 512), fill_value=1.0),
+        np.ones(shape=(1, 1, 512)),
+    )
+
+
+@pytest.mark.parametrize(
+    argnames="seed",
+    argvalues=[0, 42],
+    ids=["seed-0", "seed-42"],
+)
+def test_ones(seed: int) -> None:
+    """Test ones function"""
+    assert np.allclose(
+        ones(key=random.PRNGKey(seed), shape=(1, 1, 512)),
+        np.ones(shape=(1, 1, 512)),
+    )
+
+
+@pytest.mark.parametrize(
+    argnames="seed",
+    argvalues=[0, 42],
+    ids=["seed-0", "seed-42"],
+)
+def test_zeros(seed: int) -> None:
+    """Test zeros function"""
+    assert np.allclose(
+        zeros(key=random.PRNGKey(seed), shape=(1, 1, 512)),
+        np.zeros(shape=(1, 1, 512)),
+    )
